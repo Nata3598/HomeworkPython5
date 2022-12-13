e7b2fd50fa4a1f43dc82966d3a8d4d2f4ca1b739
NATALIA <natalia.dmitrievna@icloud.com>
2022-12-13 10:20:06 +0300

    Добавление изменений с заданием №3

--- Task3.py
+++ Task3.py
@@ -1,6 +1,4 @@
-# Создайте программу для игры в ""Крестики-нолики"".
-
-board = list(range(1,10))
+# Создайте программу для игры в "Крестики-нолики".
 
 def draw_board(board):
     print ("-" * 13)
@@ -11,15 +9,16 @@ def draw_board(board):
 def take_input(player_token):
     valid = False
     while not valid:
-        player_answer = input("Куда поставим " + player_token+"? ")
-        try:
-            player_answer = int(player_answer)
-        except:
-            print ("Некорректный ввод. Вы уверены, что ввели число?")
+        player_answer = input("Куда поставим " + player_token + "? ")
+        if (player_answer == ''):
+            print ("Некорректный ввод")
             continue
+
+        player_answer = int(player_answer)
+
         if player_answer >= 1 and player_answer <= 9:
-            if (str(board[player_answer-1]) not in "XO"):
-                board[player_answer-1] = player_token
+            if ( str( board[ player_answer-1 ] ) not in "XO" ):
+                board[ player_answer-1 ] = player_token
                 valid = True
             else:
                 print ("Эта клеточка уже занята")
@@ -33,25 +32,26 @@ def check_win(board):
             return board[each[0]]
     return False
 
-def main(board):
-    counter = 0
-    win = False
-    while not win:
-        draw_board(board)
-        if counter % 2 == 0:
-            take_input("X")
-        else:
-            take_input("O")
-        counter += 1
-        if counter > 4:
-            tmp = check_win(board)
-            if tmp:
-                print (tmp, "выиграл!")
-                win = True
-                break
-        if counter == 9:
-            print ("Ничья!")
-            break
-    draw_board(board)
 
-main(board)
+board = list(range(1,10))
+counter = 0
+win = False
+
+while not win:
+    draw_board(board)
+    if counter % 2 == 0:
+        take_input("X")
+    else:
+        take_input("O")
+    counter += 1
+    if counter > 4:
+        tmp = check_win(board)
+        if tmp:
+            print (tmp, "Выиграл!")
+            win = True
+            break
+    if counter == 9:
+        print ("Ничья!")
+        break
+
+draw_board(board)
